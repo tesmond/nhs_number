4916d7b223275abd04a8d0af9c00426ed63fefb2
Andrew Gill <tesmond@gmail.com>
2022-05-31 22:33:14 +0100

    remove verbose comments

--- nhs_number.py
+++ nhs_number.py
@@ -11,25 +11,20 @@ def is_valid_nhs_number(nhs_number: str, ignore_formatting: bool = False) -> boo
     if not re.match(r"^\d{10}$", nhs_number):
         return False
 
-    # Step 1
     # Multiply each of the first nine digits by a weighting factor
     check_digit: int = 0
     for i, digit in enumerate(nhs_number):
-        # Step 2
         # Add the results of each multiplication together.
         check_digit += int(digit) * (10 - i)
 
-    # Step 3
     # Divide the total by 11 and establish the remainder.
     remainder = check_digit % 11
 
-    # Step 4
     # Subtract the remainder from 11 to give the check digit.
     check_digit = 11 - remainder
     if check_digit == 11:
         check_digit = 0
 
-    # Step 5
     # Check the remainder matches the check digit.
     # If it does not, the NHS NUMBER is invalid.
     if remainder != check_digit:
